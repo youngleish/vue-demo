c8a0a0cea4f166a73c8d9b1408ef27fd0b1761f6
杨磊 <youngleish@gmail.com>
2019-08-05 23:19:20 +0800

    动态添加路由demo

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router) // 引入router插件
 
@@ -10,20 +11,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
-      children: [
-        {path:'', name: 'list', component: () => import('./views/List.vue')},
-        {path:'/detail/:id', name: 'detail', component: () => import('./views/Detail.vue'), props: true},
-      ]
-    },
-    {
-      path: '/list',
-      name: 'list',
-      // route level code-splitting 路由层级代码分割
-      // this generates a separate chunk (about.[hash].js) for this route  这种方式会生成about.[hash].js的文件，
-      // which is lazy-loaded when the route is visited. 可以让访问路由的时候进行懒加载
-      component: () => import(/* webpackChunkName: "about" */ './views/List.vue')
+      name: 'login',
+      component: Login
     }
+    // {
+    //   path: '/list',
+    //   name: 'list',
+    //   // route level code-splitting 路由层级代码分割
+    //   // this generates a separate chunk (about.[hash].js) for this route  这种方式会生成about.[hash].js的文件，
+    //   // which is lazy-loaded when the route is visited. 可以让访问路由的时候进行懒加载
+    //   component: () => import(/* webpackChunkName: "about" */ './views/List.vue')
+    // }
   ]
 })
